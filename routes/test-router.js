9b0e33f063fae2876e2d775f4dcc691ef7be5dd9
TylerKingsley <tyler@kingsleyrobins.com>
2024-05-28 10:04:10 +0200

    updated views

--- routes/test-router.js
+++ routes/test-router.js
@@ -7,17 +7,17 @@ router.get('/', (req, res) => {
 
 router.use('/cancellation', (req, res) => {
     console.log('cancellation');
-    res.render(`test-views/standard-template`);
+    res.render(`test-views/cancellation`);
 });
 
 router.use('/claims', (req, res) => {
     console.log('claims');
-    res.render(`test-views/standard-template`);
+    res.render(`test-views/claims`);
 });
 
 router.use('/debit-order', (req, res) => {
     console.log('debit-order');
-    res.render(`test-views/standard-template`);
+    res.render(`test-views/debit-order`);
 });
 
 
